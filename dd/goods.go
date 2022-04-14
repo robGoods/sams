1487f008e9bfddd2f29fe6a80717c965831a33ee
gyf19 <gyf19@163.com>
2022-04-14 18:20:09 +0800

    统一 http Header
    1.  conf 配置结构体
    2.  完善 http Header 参数
    3.  统一 NewRequest 方法
    4. commitPay 接口 http Header 增加 track-info 参数
    5. 打印配送时间 有bug

--- dd/goods.go
+++ dd/goods.go
@@ -1,13 +1,12 @@
 package dd
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/tidwall/gjson"
 	"io/ioutil"
-	"net/http"
+
+	"github.com/tidwall/gjson"
 )
 
 type Goods struct {
@@ -61,7 +60,7 @@ func (s *DingdongSession) CheckGoods() error {
 	data["storeId"] = ""
 	goods := make([]Goods, 0)
 	for _, v := range s.Cart.FloorInfoList {
-		if v.FloorId == s.FloorId {
+		if v.FloorId == s.Conf.FloorId {
 			for _, v := range v.NormalGoodsList {
 				if data["storeId"] == "" {
 					data["storeId"] = v.StoreId
@@ -72,21 +71,8 @@ func (s *DingdongSession) CheckGoods() error {
 	}
 	data["goodsList"] = goods
 	dataStr, _ := json.Marshal(data)
-	req, _ := http.NewRequest("POST", urlPath, bytes.NewReader(dataStr))
-	req.Header.Set("Host", "api-sams.walmartmobile.cn")
-	req.Header.Set("content-type", "application/json")
-	req.Header.Set("accept", "*/*")
-	//req.Header.Set("auth-token", "xxxxxxxxxxxx")
-	req.Header.Set("auth-token", s.AuthToken)
-	//req.Header.Set("app-version", "5.0.46.1")
-	req.Header.Set("device-type", "ios")
-	req.Header.Set("Accept-Language", "zh-Hans-CN;q=1, en-CN;q=0.9, ga-IE;q=0.8")
-	//req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	//req.Header.Set("apptype", "ios")
-	//req.Header.Set("device-name", "iPhone12,8")
-	//req.Header.Set("device-os-version", "13.4.1")
-	req.Header.Set("User-Agent", "SamClub/5.0.46 (iPhone; iOS 13.4.1; Scale/2.00)")
-	req.Header.Set("system-language", "CN")
+
+	req := s.NewRequest("POST", urlPath, dataStr)
 
 	resp, err := s.Client.Do(req)
 	if err != nil {
