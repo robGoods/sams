2ca549fdcfe5c53bb679626c417e2bcd4e695db5
iscod <iscodd@gmail.com>
2022-04-16 23:27:35 +0800

    flag

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
 	"github.com/robGoods/sams/dd"
 )
 
+var (
+	showHelp     = flag.Bool("help", false, "show help")
+	authToken    = flag.String("authToken", "", "必选, Sam's App HTTP头部auth-token")
+	barkId       = flag.String("barkId", "", "可选，通知用的`bark` id, 可选参数")
+	floorId      = flag.Int("floorId", 1, "可选，1,普通商品 2,全球购保税 3,特殊订购自提 4,大件商品 5,厂家直供商品 6,特殊订购商品 7,失效商品")
+	deliveryType = flag.Int("deliveryType", 2, "可选，1 急速达，2， 全程配送")
+	longitude    = flag.String("longitude", "", "可选，HTTP头部longitude")
+	latitude     = flag.String("latitude", "", "可选，HTTP头部latitude")
+	deviceId     = flag.String("deviceId", "", "可选，HTTP头部device-id")
+	trackInfo    = flag.String("trackInfo", "", "可选，HTTP头部track-info")
+)
+
 func main() {
+	flag.Parse()
+	if *showHelp {
+		flag.Usage()
+		return
+	}
+
+	if *authToken == "" {
+		flag.Usage()
+		return
+	}
+
 	session := dd.DingdongSession{}
 	conf := dd.Config{
-		AuthToken:    "xxxxx", //HTTP头部auth-token
-		BarkId:       "",      //通知用的bark id，下载bark后从app界面获取, 如果不需要可以填空字符串
-		FloorId:      1,       //1,普通商品 2,全球购保税 3,特殊订购自提 4,大件商品 5,厂家直供商品 6,特殊订购商品 7,失效商品
-		DeliveryType: 2,       //1 急速达，2， 全程配送
-		Longitude:    "", //HTTP头部longitude,可选参数
-		Latitude:     "", //HTTP头部latitude,可选参数
-		Deviceid:     "", //HTTP头部device-id,可选参数
-		Trackinfo:    "", // HTTP头部track-info,可选参数
+		AuthToken:    *authToken,    //HTTP头部auth-token
+		BarkId:       *barkId,       //通知用的bark id，下载bark后从app界面获取, 如果不需要可以填空字符串
+		FloorId:      *floorId,      //1,普通商品 2,全球购保税 3,特殊订购自提 4,大件商品 5,厂家直供商品 6,特殊订购商品 7,失效商品
+		DeliveryType: *deliveryType, //1 急速达，2， 全程配送
+		Longitude:    *longitude,    //HTTP头部longitude,可选参数
+		Latitude:     *latitude,     //HTTP头部latitude,可选参数
+		Deviceid:     *deviceId,     //HTTP头部device-id,可选参数
+		Trackinfo:    *trackInfo,    // HTTP头部track-info,可选参数
 	}
 
 	err := session.InitSession(conf)
